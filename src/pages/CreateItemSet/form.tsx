0f4208093e6ec91dfbeace7084cf1d7d9c6681fb
ngustavin <gust4f3rn@gmail.com>
2020-12-07 09:28:14 -0300

    Formik Added to create-set

--- src/pages/CreateItemSet/form.tsx
+++ src/pages/CreateItemSet/form.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../../components/Header/index'
-import { Container, Main, TitleArea, Form } from '../../styles/pages/form'
+import { Container, Main, TitleArea } from '../../styles/pages/form'
+import 
 
 
 export default function ItemSetForm() { 
   
-
+  const onSubmit = (data: any) => console.log(data)
 
   return(
+
+
     <>
     <Container>
     <Header haveArrowLeft/>
@@ -18,41 +21,7 @@ export default function ItemSetForm() {
         </div>
         {/* <label  ref="file"> Choose a File </label> */}
       </TitleArea>
-      <Form id = "form-container">
-
-        <section className="scroll-container">
-          <h1 className="form-title">Cadastrar Set de item</h1>
-
-          <div className="field-container">
-            <h1>Nome</h1>
-            <input type="text" />
-          </div>
-
-          <div className="two-field-container">
-            <div>
-              <h1>Disponível</h1>
-              <input type="text"/>
-          </div>
-
-            <div>
-              <h1>Quantidade</h1>
-              <input type="text"/>
-            </div>
-          </div>
-        
-          <div className="field-container">
-            <h1>Descrição</h1>
-            <input type="text"/>
-          </div>
-
-        </section>
-
-        <div className="send-data-container">
-          <input type="button" className="add-field" value="Adicionar Campo Personalizado" onClick={() => {console.log('hello')}}/>
-          <input type="button" className="add-field" value="Cadastrar modelo de Item" />
-        </div>
-
-      </Form>
+      
     </Main>
     </Container> 
     </>
